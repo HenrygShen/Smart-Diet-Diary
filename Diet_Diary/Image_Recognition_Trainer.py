9e482259520c4f3f4f6511184011a347985ddcf3
Henry Shen <hen.2005@hotmail.com>
2019-04-09 17:06:42 +1200

    Added training

--- Diet_Diary/Image_Recognition_Trainer.py
+++ Diet_Diary/Image_Recognition_Trainer.py
@@ -4,35 +4,36 @@ from keras.layers import Dense, Dropout, Flatten
 from keras.layers.convolutional import Conv2D, MaxPooling2D
 from keras.optimizers import SGD
 from keras.constraints import maxnorm
+import food101
 
 from keras.datasets import cifar10
 
-(X_train, y_train), (X_test, y_test) = cifar10.load_data()
-print(y_test)
+(X_train, y_train)  = food101.load_data()
+
+
+from keras.utils import np_utils
+
+new_X_train = X_train.astype('float32')
+
+new_X_train /= 255
+print(X_train)
+
+new_Y_train = np_utils.to_categorical(y_train)
+
+
 #
-# from keras.utils import np_utils
+# #
+model = Sequential()
+model.add(Conv2D(32, (3, 3), input_shape=(50, 50, 3), activation='relu', padding='same', kernel_constraint=maxnorm(3)))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Flatten())
+model.add(Dense(1024, activation='relu', kernel_constraint=maxnorm(3)))
+model.add(Dropout(0.5))
+model.add(Dense(101, activation='softmax'))
 #
-# new_X_train = X_train.astype('float32')
-# new_X_test = X_test.astype('float32')
-# new_X_train /= 255
-# new_X_test /= 255
-#
-# new_Y_train = np_utils.to_categorical(y_train)
-# new_Y_test = np_utils.to_categorical(y_test)
-#
-#
-#
-# model = Sequential()
-# model.add(Conv2D(32, (3, 3), input_shape=(32, 32, 3), activation='relu', padding='same', kernel_constraint=maxnorm(3)))
-# model.add(MaxPooling2D(pool_size=(2, 2)))
-# model.add(Flatten())
-# model.add(Dense(512, activation='relu', kernel_constraint=maxnorm(3)))
-# model.add(Dropout(0.5))
-# model.add(Dense(10, activation='softmax'))
-#
-# model.compile(loss='categorical_crossentropy', optimizer=SGD(lr=0.01), metrics=['accuracy'])
-# model.fit(new_X_train, new_Y_train, epochs=10, batch_size=32)
+model.compile(loss='categorical_crossentropy', optimizer=SGD(lr=0.01), metrics=['accuracy'])
+model.fit(new_X_train, new_Y_train, epochs=100, batch_size=50)
 #
 #
-# import h5py
-# model.save('Trained_model.h5')
+import h5py
+model.save('Trained_model.h5')
--- Diet_Diary/food101.py
+++ Diet_Diary/food101.py
@@ -1,33 +1,89 @@
 import pathlib
 import json
-
+import numpy
 # Imports for debugging
 import matplotlib.pyplot as plt
+import tensorflow as tf
 from PIL import Image
 
 
 # Set root for images
-data_root = pathlib.Path('E:/Downloads/food-101')
+# data_root = pathlib.Path('E:/Downloads/food-101/images')
 
 
-# Get images for apple pies
-all_image_paths = list(data_root.glob("images/apple_pie/*"))
-all_image_paths = [str(path) for path in all_image_paths]
+# # Get images for apple pies
+# all_image_paths = list(data_root.glob("/apple_pie/*"))
+# all_image_paths = [str(path) for path in all_image_paths]
+#
+#
+# # Show image for apple pie - DEBUGGING
+# image_path = all_image_paths[900]
+# im = Image.open(image_path, 'r')
+# image = plt.imread(image_path)
+# plt.imshow(image)
+# plt.show()
 
 
-# Show image for apple pie - DEBUGGING
-image_path = all_image_paths[900]
-im = Image.open(image_path, 'r')
-image = plt.imread(image_path)
-plt.imshow(image)
-plt.show()
+def get_class_labels():
+    # Read labels
+    with open("E:/Downloads/food-101/meta/classes.json", 'r') as file:
+        labels_dict = json.load(file)
+        labels = labels_dict['classes']
+
+    # Print labels with indices
+    labels = dict((name, index) for index, name in enumerate(labels))
+
+    return labels
 
 
-# Read labels
-with open("E:/Downloads/food-101/meta/labels.json", 'r') as file:
-    labels_dict = json.load(file)
-    labels = labels_dict['labels']
+def get_train_labels():
 
-# Print labels with indices
-label_to_index = dict((name, index) for index,name in enumerate(labels))
-print(label_to_index)
+    # Read labels
+    with open("E:/Downloads/food-101/meta/test.json", 'r') as file:
+        labels = json.load(file)
+
+    return labels
+
+
+def load_data():
+
+    # Set up results
+    x_train = []
+    y_train = []
+
+    # Get labels
+    class_labels = get_class_labels()
+    train_labels = get_train_labels()
+
+    for (k, v) in train_labels.items():
+        for i in range(0, len(v)):
+            # Format image
+            # TODO: process
+            image = Image.open("E:/Downloads/food-101/images/" + v[i] + ".jpg")
+            image = image.resize((50, 50), Image.ANTIALIAS)
+
+            image.load()
+            data = numpy.asarray(image, dtype="uint8")
+
+            image.close()
+            # print(len(data))
+            # print(data)
+            # Append image
+            x_train.append(data)
+            y_train.append(class_labels[k])
+
+    # print(x_train)
+    x_train = numpy.asarray(x_train, dtype  ='uint8')
+    # write(x_train)
+    return x_train, y_train;
+
+
+# image = Image.open("E:/Downloads/food-101/images/apple_pie/134.jpg")
+# image = image.resize((200, 200), Image.ANTIALIAS)
+# image.load()
+# data = numpy.asarray(image, dtype="int32" )
+# # image.save('sompic.jpg')
+# # image = plt.imread("E:/Downloads/food-101/images/apple_pie/134.jpg")
+#
+# plt.imshow(data)
+# plt.show()
