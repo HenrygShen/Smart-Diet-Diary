2142c9ea6e1fc23ece4980f4a38da65282a3c521
unknown <lincoln.choy1997@gmail.com>
2019-04-17 14:05:42 +1200

    Added routes to diary and photo screen

--- app-SmartDietDiary/App.js
+++ app-SmartDietDiary/App.js
@@ -4,14 +4,19 @@ import { Provider } from 'react-redux';
 import LoginScreen from './src/screens/Login/Login';
 
 import configureStore from './src/store/configureStore';
-// import SideDrawer from './src/screens/SideDrawer/SideDrawer';
+import DiaryScreen from './src/screens/Diary/Diary';
+import PhotoScreen from './src/screens/Photo/Photo';
+import SideDrawer from './src/screens/SideDrawer/SideDrawer';
 
 const store = configureStore();
 
 /* Register screens */
 Navigation.registerComponent("sdd.LoginScreen", () => LoginScreen, store, Provider);
+Navigation.registerComponent("sdd.DiaryScreen", () => DiaryScreen, store, Provider);
+Navigation.registerComponent("sdd.PhotoScreen", () => PhotoScreen, store, Provider);
 
-// Navigation.registerComponent("places.SideDrawer", () => SideDrawer, store, Provider);
+
+Navigation.registerComponent("sdd.SideDrawer", () => SideDrawer, store, Provider);
 
 /* Start app */
 Navigation.startSingleScreenApp({
---
+++ app-SmartDietDiary/src/screens/Photo/Photo.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+
+
+class PhotoScreen extends React.Component {
+
+    static navigatorStyle = {
+        navBarButtonColor: 'orange'
+    };
+
+    constructor(props) {
+        super(props);
+        this.props.navigator.setOnNavigatorEvent(this.onNavigatorEvent.bind(this));
+    }
+
+    onNavigatorEvent = (event) => {
+        if (event.type === 'NavBarButtonPress') {
+            if (event.id === 'sideDrawerToggle') {
+                this.props.navigator.toggleDrawer({
+                    side: 'left',
+                    animated: true,
+                    to: 'open'
+                });
+            }
+        }
+    }
+
+    
+    render() {
+
+        return (
+            <View>
+                <Text>
+                    Hi photo
+                </Text>
+            </View>
+        )
+    }
+}
+
+export default PhotoScreen;
