c7ee1b5f06c40112f39cbddb04a25c53558f4eda
Lincoln <lincoln.choy1997@gmail.com>
2019-09-06 15:30:36 +1200

    Implement profile edit screen

--- App_SDD/src/screens/ProfileScreen/ProfileScreen.js
+++ App_SDD/src/screens/ProfileScreen/ProfileScreen.js
@@ -1,9 +1,19 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
-import { getDetails } from '../../utility/database';
+import { View, StyleSheet } from 'react-native';
+import RadioForm from 'react-native-simple-radio-button';
+
+import { getDetails, saveUserData } from '../../utility/database';
 import MainText from '../../components/UI/MainText/MainText';
 import Button from '../../components/UI/Button/Button';
 import DefaultInput from '../../components/UI/DefaultInput/DefaultInput';
+
+const radio_props = [
+    {label: 'Little to no exercise', value: 0 },
+    {label: 'Exercise 1 - 3 days/week', value: 1 },
+    {label: 'Exercise 3 - 5 days/week', value: 2 },
+    {label: 'Exercise 6 - 7 days/week', value: 3 },
+    {label: 'Very hard exercise all week', value: 4 }
+]
 
 
 class ProfileScreen extends React.Component {
@@ -14,28 +24,49 @@ class ProfileScreen extends React.Component {
             details: {
                 age: null,
                 height: null,
-                weight: null
+                weight: null,
+                activity: {
+                    label: '',
+                    value: 0
+                }
             },
             controls: {
                 age: null,
                 height: null,
-                weight: null
+                weight: null,
+                activity: null
             },
             editMode: false
         }
     }
 
     componentDidMount() {
+        this.loadDetails();
+    }
+
+    loadDetails = () => {
         getDetails()
         .then(rows => {
             const details = rows.item(0);
             this.setState(prevState => {
                 return {
-                    ...prevState,
+                    controls: {
+                        age: details.Age.toString(),
+                        weight: details.Weight.toString(),
+                        height: details.Height.toString(),
+                        activity: {
+                            label: radio_props[parseInt(details.Activity)].label,
+                            value: radio_props[parseInt(details.Activity)].value
+                        }
+                    },
                     details: {
                         age: details.Age.toString(),
                         weight: details.Weight.toString(),
-                        height: details.Height.toString()
+                        height: details.Height.toString(),
+                        activity: {
+                            label: radio_props[parseInt(details.Activity)].label,
+                            value: radio_props[parseInt(details.Activity)].value
+                        }
                     }
                 }
             })
@@ -46,9 +77,46 @@ class ProfileScreen extends React.Component {
         this.setState({ editMode: !this.state.editMode});
     }
 
+    onChangeText = (text, type) => {
+        this.setState(prevState => { 
+            return {
+                ...prevState,
+                controls: {
+                    ...prevState.controls,
+                    [type]: text
+                }
+            }   
+        });
+    }
+
+    onUpdateActivity = (value) => {
+        this.setState(prevState => {
+            return {
+                ...prevState,
+                controls: {
+                    ...prevState.controls,
+                    activity: {
+                        value: value,
+                        label: radio_props[value]
+                    }
+                }
+            }
+        })
+    }
+
+    onSave = () => {
+        saveUserData(this.state.controls.weight, this.state.controls.height, this.state.controls.age, this.state.controls.activity.value)
+        .then(() => {
+            this.onToggleEdit();
+            this.loadDetails();
+            alert('Details saved');
+        });
+        
+    }
+
     render() {
 
-        const { age, height, weight } = this.state.details;
+        const { age, height, weight, activity } = this.state.details;
 
         let mainContent;
         if (!this.state.editMode) {
@@ -64,6 +132,9 @@ class ProfileScreen extends React.Component {
                 <View style = {styles.inputContainer}>
                     <MainText style = {{fontSize: 15}}>Weight: {weight} KG</MainText>
                 </View>
+                <View style = {styles.inputContainer}>
+                    <MainText style = {{fontSize: 15}}>Activity: {activity.label}</MainText>
+                </View>
                 <Button style = {{width: '50%', alignSelf: 'center'}} onPress = {this.onToggleEdit}>Edit</Button>
             </View>
         }
@@ -72,16 +143,53 @@ class ProfileScreen extends React.Component {
             <View style = {styles.infoContainer}>
                 <MainText style = {{fontSize: 25}}>Edit information</MainText>
                 <View style = {styles.inputContainer}>
-                    <DefaultInput placeholder = 'Age' value = {this.state.controls.age} style = {{fontSize: 15}}/>
+                    <MainText style = {{fontSize: 15}}>Age</MainText>
+                    <DefaultInput 
+                        placeholder = 'Age' 
+                        onChangeText = { (text) => this.onChangeText(text, 'age')} 
+                        value = {this.state.controls.age} 
+                        style = {{fontSize: 15}} 
+                        keyboardType ={'numeric'}
+                        defaultValue = {this.state.controls.age}
+                    />
                 </View>
                 <View style = {styles.inputContainer}>
-                    <DefaultInput placeholder = 'Height' value = {this.state.controls.height} style = {{fontSize: 15}}/>
+                    <MainText style = {{fontSize: 15}}>Height (CM)</MainText>
+                    <DefaultInput 
+                        placeholder = 'Height' 
+                        onChangeText = { (text) => this.onChangeText(text, 'height')}  
+                        value = {this.state.controls.height} 
+                        style = {{fontSize: 15}} 
+                        keyboardType ={'numeric'}
+                        defaultValue = {this.state.controls.height}
+                    />
                 </View>
                 <View style = {styles.inputContainer}>
-                    <DefaultInput placeholder = 'Weight' value = {this.state.controls.weight} style = {{fontSize: 15}}/>
+                    <MainText style = {{fontSize: 15}}>Weight (KG)</MainText>
+                    <DefaultInput 
+                        placeholder = 'Weight' 
+                        onChangeText = { (text) => this.onChangeText(text, 'weight')}  
+                        value = {this.state.controls.weight} 
+                        style = {{fontSize: 15}} 
+                        keyboardType ={'numeric'}
+                        defaultValue = {this.state.controls.weight}
+                    />
                 </View>
-                <View style = {{flexDirection: 'row', justifyContent: 'space-around'}}>
-                    <Button style = {{width: '38%'}} onPress = {this.onToggleEdit}>Save</Button>
+                <View>
+                <RadioForm
+                    radio_props={[
+                        {label: 'Little to no exercise', value: 0 },
+                        {label: 'Exercise 1 - 3 days/week', value: 1 },
+                        {label: 'Exercise 3 - 5 days/week', value: 2 },
+                        {label: 'Exercise 6 - 7 days/week', value: 3 },
+                        {label: 'Very hard exercise all week', value: 4 }
+                    ]}
+                    initial={activity.value}
+                    onPress={(value) => { this.onUpdateActivity(value)}}
+                    />
+                </View>
+                <View style = {{flexDirection: 'row', justifyContent: 'space-around', marginTop: 20}}>
+                    <Button style = {{width: '38%'}} onPress = {this.onSave}>Save</Button>
                     <Button style = {{width: '38%'}} onPress = {this.onToggleEdit}>Cancel</Button>
                 </View>
             </View>
